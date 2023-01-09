72c2b42a79afc37955632dc1cd01856c23ca6da6
cartersalna <87328506+cartersalna@users.noreply.github.com>
2023-01-09 11:31:24 -0500

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+
+int NumberOfCharactersOverlap(std::string s1, std::string s2);
+
+std::string CombineWords(std::string string1, std::string string2, int number);
+
+std::string TurnSentenceIntoWord(std::string str, int wordPlace);
+
+int NumberOfWords(std::string sentence);
+
+#endif
